4c39101c6bd977e7e5172f8d697c85370033cb81
Cesar <cesar@existo.es>
2019-07-28 19:24:37 +0200

    autorespuesta

--- postfix/existo/3.4.5-3/files/opt/postfix/autorespuesta/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const from = process.argv [2];
-const to   = process.argv [3];
-
-console.log ('=============================');
-console.log (`mail from ${ from } to ${ to }`);
-
